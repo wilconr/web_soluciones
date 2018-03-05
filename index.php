2362b2e91435c6489a8b8e8eff2ba64987f46884
wilmer contreras <wraulcontreras@gmail.com>
2018-03-05 17:43:51 -0500

    arreglos en menores

--- index.php
+++ index.php
@@ -794,7 +794,7 @@ if ($_POST) {
           header: null, // Selector for fixed headers (must be a valid CSS selector)
 
           // Speed & Easing
-          speed: 500, // Integer. How fast to complete the scroll in milliseconds
+          speed: 2000, // Integer. How fast to complete the scroll in milliseconds
           offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
           easing: 'easeInOutCubic', // Easing pattern to use
           customEasing: function (time) {
